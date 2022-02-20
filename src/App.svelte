5d0150e9269ae6d7bf30dc9d2d140d354ea621c1
Doltons Edward Nicholas <75367447+doltonsedward@users.noreply.github.com>
2022-02-20 19:57:04 +0800

    setup conditional rendering for trash icon

--- src/App.svelte
+++ src/App.svelte
@@ -55,32 +55,41 @@
 		};
 
 		todos = [todo, ...todos];
-    setTodos(todos)
+    	setTodos(todos)
 		input.value = '';
 	}
 
 	function remove(todo) {
 		todos = todos.filter(t => t !== todo);
-    setTodos(todos)
+    	setTodos(todos)
 	}
 
 	function mark(todo, done) {
 		todo.done = done;
 		remove(todo);
 		todos = todos.concat(todo);
-    setTodos(todos)
+    	setTodos(todos)
+	}
+
+	let isDarkTheme = (localStorage.getItem("theme") === "dark")
+
+	/**
+	* @param {{ detail: { isDark: boolean; }; }} event
+	*/
+	function handleGetTheme(event) {
+		isDarkTheme = event.detail.isDark
 	}
 </script>
 
 <div class="board">
-	<ThemePicker />
+	<ThemePicker on:getTheme={handleGetTheme} />
 	
 	<input
 		placeholder="what needs to be done?"
 		on:keydown={e => e.key === 'Enter' && add(e.target)}
 	>
 
-	<div class="left">
+	<div class="section-left">
 		<h2>todo</h2>
 		{#each todos.filter(t => !t.done) as todo (todo.id)}
 			<label
@@ -90,12 +99,18 @@
 			>
 				<input type=checkbox on:change={() => mark(todo, true)} />
 				{todo.description}
-				<button on:click="{() => remove(todo)}">remove</button>
+				<button on:click="{() => remove(todo)}">
+					{#if isDarkTheme}
+						<img src="images/trash-light.png" alt="delete" />
+					{:else}
+						<img src="images/trash.png" alt="delete" />
+					{/if}
+				</button>
 			</label>
 		{/each}
 	</div>
 
-	<div class="right">
+	<div class="section-right">
 		<h2>done</h2>
 		{#each todos.filter(t => t.done) as todo (todo.id)}
 			<label
@@ -106,7 +121,13 @@
 			>
 				<input type=checkbox checked on:change={() => mark(todo, false)}>
 				{todo.description}
-				<button on:click="{() => remove(todo)}">remove</button>
+				<button on:click="{() => remove(todo)}">
+					{#if isDarkTheme}
+						<img src="images/trash-light.png" alt="delete" />
+					{:else}
+						<img src="images/trash.png" alt="delete" />
+					{/if}
+				</button>
 			</label>
 		{/each}
 	</div>
@@ -114,6 +135,13 @@
 
 <style>
   	:focus { outline: none }
+	::placeholder {
+		opacity: 0.5;
+	}
+	  
+	::placeholder, .board > input, label {
+		color: var(--text-color);
+	}
 
 	.board {
 		display: grid;
@@ -137,6 +165,7 @@
 	}
 
 	h2 {
+		color: whitesmoke;
 		font-size: 2em;
 		font-weight: 200;
 		user-select: none;
@@ -144,7 +173,7 @@
 	}
 
 	label {
-    display: block;
+    	display: block;
 		position: relative;
 		line-height: 1.2;
 		padding: 0.5em 2.5em 0.5em 0.5em;
@@ -156,7 +185,6 @@
 		background-color: var(--neumorphism-bg);
 		border-radius: 12px;
 		border: 1px solid var(--neumorphism-border-color);
-		color: #333;
 	}
 
 	.done {
@@ -168,19 +196,22 @@
 	}
 
 	button {
+		display: grid;
+		place-items: center;
 		position: absolute;
 		top: 0;
 		right: 0.2em;
 		width: 2em;
 		height: 100%;
-		background: no-repeat 50% 50% url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'%3E%3Cpath fill='%23676778' d='M12,2C17.53,2 22,6.47 22,12C22,17.53 17.53,22 12,22C6.47,22 2,17.53 2,12C2,6.47 6.47,2 12,2M17,7H14.5L13.5,6H10.5L9.5,7H7V9H17V7M9,18H15A1,1 0 0,0 16,17V10H8V17A1,1 0 0,0 9,18Z'%3E%3C/path%3E%3C/svg%3E");
+		background-color: transparent;
 		background-size: 1.4em 1.4em;
 		border: none;
 		opacity: 0;
 		transition: opacity 0.2s;
-		text-indent: -9999px;
 		cursor: pointer;
 	}
+
+	button img { width: 20px }
 
 	label:hover button { opacity: 1 }
 </style>
--- src/lib/ThemePicker.svelte
+++ src/lib/ThemePicker.svelte
@@ -1,18 +1,23 @@
 <script context="module">
 	const currentTheme = localStorage.getItem("theme");
 	if (currentTheme) 
-		document.getElementsByTagName("body")[0].classList.add(currentTheme)
+	document.getElementsByTagName("body")[0].classList.add(currentTheme)
 </script>
 <script>
+	import { createEventDispatcher } from "svelte";
+	const dispatch = createEventDispatcher()
+	
 	/**
 	* @param {boolean} isDark
 	*/
 	function setDarkTheme(isDark) {
 		if (isDark) {
 			localStorage.setItem("theme", "dark")
+			dispatch("getTheme", { isDark: true })
 			document.getElementsByTagName("body")[0].classList.add("dark")
 		} else {
 			localStorage.setItem("theme", "")
+			dispatch("getTheme", { isDark: true })
 			document.getElementsByTagName("body")[0].classList.remove("dark")
 		}
 	}
