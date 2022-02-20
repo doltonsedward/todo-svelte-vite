96bc11cfffa2a24ae01a32c471ed9b32d4285204
Doltons Edward Nicholas <75367447+doltonsedward@users.noreply.github.com>
2022-02-20 17:32:43 +0800

    create ThemePicker component

---
+++ src/lib/ThemePicker.svelte
@@ -0,0 +1,25 @@
+<script context="module">
+	const currentTheme = localStorage.getItem("theme");
+	if (currentTheme) 
+		document.getElementsByTagName("body")[0].classList.add(currentTheme)
+</script>
+<script>
+	/**
+	* @param {boolean} isDark
+	*/
+
+	function setDarkTheme(isDark) {
+		if (isDark) {
+			localStorage.setItem("theme", "dark")
+			document.getElementsByTagName("body")[0].classList.add("dark")
+		} else {
+			localStorage.setItem("theme", "")
+			document.getElementsByTagName("body")[0].classList.remove("dark")
+		}
+	}
+</script>
+
+<div>
+	<div on:click={()=> setDarkTheme(false)}>Light</div>
+	<div on:click={()=> setDarkTheme(true)}>Dark</div>
+</div>
