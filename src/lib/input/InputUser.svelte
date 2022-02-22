bd924260b7f1a74c16613138b8684563f686aba3
Doltons Edward Nicholas <75367447+doltonsedward@users.noreply.github.com>
2022-02-22 21:17:57 +0800

    create Input User component

---
+++ src/lib/input/InputUser.svelte
@@ -0,0 +1,16 @@
+<input placeholder="what needs to be done?" on:keydown />
+
+<style>
+    input {
+		font-family: "Poppins";
+		margin-top: 12px;
+		padding: 0.5em 1em;
+		font-size: 1.4em;
+		grid-column: 1/3;
+		backdrop-filter: blur(16px) saturate(180%);
+		-webkit-backdrop-filter: blur(16px) saturate(180%);
+		background-color: var(--neumorphism-bg);
+		border-radius: 12px;
+		border: 1px solid var(--neumorphism-border-color);
+	}
+</style>
