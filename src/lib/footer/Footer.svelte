43f47c153d82182d70c9a1de197959e34ad1e57b
Doltons Edward Nicholas <75367447+doltonsedward@users.noreply.github.com>
2022-02-28 20:41:02 +0800

    fix styling in Footer component
    - integrate Setting component

--- src/lib/footer/Footer.svelte
+++ src/lib/footer/Footer.svelte
@@ -2,6 +2,8 @@
     import { createEventDispatcher } from "svelte";
     import Setting from "./Setting.svelte";
 
+    let isSettingOpen = false
+
     const dispatch = createEventDispatcher();
 
     let editText = false
@@ -16,25 +18,43 @@
 </script>
 
 <footer>
-    <Setting />
-    <div class="container">
-        <div class="d-flex-responsive" on:click={handleDispatch}>
-            <p>Edit Text</p>
-            <input class="checkbox" type=checkbox />
+    <div class="inner-footer">
+        <Setting {isSettingOpen} on:click={()=> isSettingOpen = !isSettingOpen} />
+        <div class="container" class:active={isSettingOpen}>
+            <div class="d-flex-responsive" on:click={handleDispatch}>
+                <p>Edit Text</p>
+                <input class="checkbox" type=checkbox />
+            </div>
         </div>
     </div>
 </footer>
 
 <style>
     footer {
+        color: var(--text-color);
         width: 100%;
-        position: absolute;
+        position: fixed;
         bottom: 0;
-        background-color: lightblue;
     }
 
+    .inner-footer {
+        padding: 0 20px;
+    }
+    
     .container {
-        padding: 0 10px 12px;
+        backdrop-filter: blur(16px) saturate(180%);
+		-webkit-backdrop-filter: blur(16px) saturate(180%);
+		background-color: var(--neumorphism-bg);
+		border-radius: 12px;
+		border: 1px solid var(--neumorphism-border-color);
+        transform: translateY(100%);
+        padding: 12px 10px 12px;
+        margin-bottom: 0;
+    }
+
+    .container.active {
+        transform: translateY(0%);
+        margin-bottom: 10px;
     }
 
     .checkbox {
