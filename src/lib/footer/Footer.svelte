311fdad04b2a5909ec8e42de720b342111dcecbd
Doltons Edward Nicholas <75367447+doltonsedward@users.noreply.github.com>
2022-02-28 19:58:15 +0800

    create Footer component
    - add dev mode here

---
+++ src/lib/footer/Footer.svelte
@@ -0,0 +1,43 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+    import Setting from "./Setting.svelte";
+
+    const dispatch = createEventDispatcher();
+
+    let editText = false
+
+    function handleDispatch() {
+        dispatch("devmode", {
+            editText: !editText
+        })
+
+        editText = !editText
+    }
+</script>
+
+<footer>
+    <Setting />
+    <div class="container">
+        <div class="d-flex-responsive" on:click={handleDispatch}>
+            <p>Edit Text</p>
+            <input class="checkbox" type=checkbox />
+        </div>
+    </div>
+</footer>
+
+<style>
+    footer {
+        width: 100%;
+        position: absolute;
+        bottom: 0;
+        background-color: lightblue;
+    }
+
+    .container {
+        padding: 0 10px 12px;
+    }
+
+    .checkbox {
+        margin-left: 10px;
+    }
+</style>
